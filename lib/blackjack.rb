db951f3e3380ca6973b367963ed704f86486fb6c
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 02:09:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,14 +40,7 @@ def hit?(current_card_total)
   prompt_user
   user_input = get_user_input
   
-  if user_input == 'h'
-    num = deal_card
-    current_card_total += num
-  end
-  if user_input != 's' && user_input != 'h'
-    invalid_command
-    prompt_user
-  end
+  
   
   display_card_total(current_card_total)
 end
