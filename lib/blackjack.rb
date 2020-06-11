cd677ffe92004e8539ea3c47c08a8bdcca666de3
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 02:37:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,14 @@ end
 
 def runner
   welcome
-  initial_round
-  hit?
-  
+  current_total = initial_round
+  next_total= hit?
+  if next_total == current_total
+    hit?
+  else display_card_total(next_total)
+    if next_total > 20
+      end_game
+    end
+  end
 end
     
