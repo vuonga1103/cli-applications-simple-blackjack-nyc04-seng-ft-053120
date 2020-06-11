3f54d05db98eece0447af5271eabe8e21b0ad728
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 01:47:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,7 +49,7 @@ def hit?(current_card_total)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts 'Please enter a valid command'
 end
 
 #####################################################
