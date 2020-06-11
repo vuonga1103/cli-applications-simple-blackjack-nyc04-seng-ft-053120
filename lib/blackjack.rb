20e2206da873403d01b13a09d9321dc421cbe2f5
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 01:39:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,3 +1,4 @@
+require "pry"
 def welcome
   puts "Welcome to the Blackjack Table"
 end
@@ -24,6 +25,7 @@ end
 
 def initial_round
   card_total = deal_card + deal_card
+  binding_pry
   display_card_total(card_total)
 end
 
