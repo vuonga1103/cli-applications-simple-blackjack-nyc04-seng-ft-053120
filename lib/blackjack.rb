2aac76d00092d40de84d0829829c89c7c5256709
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 02:44:12 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,52 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  card1 = deal_card
+  card2 = deal_card
+  display_card_total(card1 + card2)
+  return card1 + card2
 end
 
 def invalid_command
-  # code invalid_command here
+  puts 'Please enter a valid command'
+end
+
+def hit?(current_card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    current_card_total += deal_card
+  elsif user_input == "s"
+    current_card_total
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+  end
 end
 
 #####################################################
@@ -39,6 +54,16 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  # Deal two cards for initial round, store score in total
+  total = initial_round
+  
+  # Keep asking if user wants to hit if total is under 21, and display the card total
+  until total > 21 
+    total = hit?(total)
+    display_card_total(total)
+  end
+  
+  # If total is > 21 end game
+  end_game(total)
 end
-    
