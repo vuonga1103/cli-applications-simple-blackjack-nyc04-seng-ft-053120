19ab21f88e367e7b841c4b09983cba53f8a67273
Anh Vuong <vuonga1103@yahoo.com>
2020-06-11 02:26:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,23 +36,17 @@ def invalid_command
 end
 
 def hit?(current_card_total)
-  
   prompt_user
   user_input = get_user_input
-  
-  if user_input != "h" && user_input != "s"
+  if user_input == "h"
+    current_card_total += deal_card
+  elsif user_input == "s"
+    current_card_total
+  else
     invalid_command
     prompt_user
   end
-  
-  if user_input == "h"
-    num = deal_card
-    current_card_total += num
-  elsif user_input == "s"
-    current_card_total = current_card_total
-  end
-  
-  current_card_total
+    
 end
 
 #####################################################
